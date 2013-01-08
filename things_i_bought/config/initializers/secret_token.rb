95a1a63b9e7a98ab248b854f9bae9181057d932b
wsugg <sugg.will@gmail.com>
2013-01-08 15:42:24 -0800

    adding chap1 things_i_bought

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = 'd229b1868e2be14fab235269fcd6cee996997aba93eddb7a4c7416c99938277a5a5ee8d72208e5f0c53835a3cfab766c69f59f9f3c61215a72d9d79a40bb9337'
